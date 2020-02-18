47925e5c4091b4ada0b4f740e4db4f824f06abe0
yh17 <60178265+yh17@users.noreply.github.com>
2020-02-17 23:47:28 -0500

    Complete assignement

---
+++ Scripts/Meteor.gd
@@ -0,0 +1,38 @@
+extends RigidBody2D
+
+var velocity = Vector2(0,0)
+export var min_speed = 100
+export var max_speed = 600
+export var damage = 50
+
+onready var Explosion = load("//res://Scenes/Explosion.tscn")
+
+func _ready():
+	randomize()
+	position.x = randi() % int(get_viewport_rect().size.x)
+	velocity.y = max(randi() % max_speed, min_speed)
+	contact_monitor = true
+	set_max_contacts_reported(4)
+	linear_velocity = velocity
+
+# warning-ignore:unused_argument
+func _physics_process(delta):
+	var colliding = get_colliding_bodies()
+	for c in colliding:
+		if c.name != "bullet2":
+		 var explosion = Explosion.instance()
+		 explosion.position = position
+		 explosion.get_node("Sprite").playing = true
+		 get_node("/root/Game/Explotions").add_child(explosion)
+		if c.name == "Player":
+			c.change_health(-damage)
+		queue_free()
+	print(position)
+	if position.y > get_viewport_rect().size.y + 50:
+		queue_free()
+	
+
+func _integrate_forces(state):
+	pass
+
+
